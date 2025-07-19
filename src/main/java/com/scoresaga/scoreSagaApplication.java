5217aec06015740600985b08d231f1af18f6eb2b
Jill Patel <pateljill2288@gmail.com>
2025-07-18 17:57:59 -0700

    renamed

--- src/main/java/com/scoresaga/scoreSagaApplication.java
+++ src/main/java/com/scoresaga/scoreSagaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScoresagaApplication {
+public class scoreSagaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScoresagaApplication.class, args);
+		SpringApplication.run(scoreSagaApplication.class, args);
 	}
 
 }
